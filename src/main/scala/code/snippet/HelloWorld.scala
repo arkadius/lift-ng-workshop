ec6f0fbf051bf12bcacc9c8f26016e734255024d
Arek Burdach <arek.burdach@gmail.com>
2015-01-17 18:22:52 +0100

    Minimum example

--- src/main/scala/code/snippet/HelloWorld.scala
+++ src/main/scala/code/snippet/HelloWorld.scala
@@ -1,40 +1,10 @@
 package code 
 package snippet
 
-import net.liftweb.http._
 import net.liftweb.util.Helpers._
 
-import scala.xml.NodeSeq
+class HelloWorld {
 
-class HelloWorld extends StatefulSnippet with RenderFuncDispatch {
+  def render = "#world" #> "TouK"
 
-  private val ajaxEnabled = S.param("ajax").flatMap(str => tryo(str.toBoolean)).openOr(false)
-
-  private var start = 1
-
-  private def numbers: Seq[String] = {
-    (start to start+4).map(_.toString)
-  }
-
-  private def nextNumbers(): Unit = {
-    start += 1
-  }
-
-  def render: NodeSeq => NodeSeq = SHtml.idMemoize { memoize =>
-    ".row *" #> numbers.map(".number *" #> _) &
-      renderSubmit(memoize)
-  }
-
-  def renderSubmit(memoize: IdMemoizeTransform) = {
-    val renderedHtml =
-      if (ajaxEnabled) {
-        SHtml.ajaxButton("Ajax", {() =>
-          nextNumbers()
-          memoize.setHtml()
-        })
-      } else {
-        SHtml.submit("Post", nextNumbers)
-      }
-    "type=submit" #> renderedHtml
-  }
 }
